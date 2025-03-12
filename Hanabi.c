2ec0ec330f427cc3bc9669d8d91b8c24f27a3f89
IsabellaSchneider7 <71522975+IsabellaSchneider7@users.noreply.github.com>
2025-03-12 19:15:42 -0400

    updated some hanabi TODOS

--- Hanabi.c
+++ Hanabi.c
@@ -17,76 +17,26 @@ printf("cardavail %d cardsplayed %d",cards_available[player_cards[player][card][
 #include <time.h>
 
 // TODO: Macro capitalized
-#define num_players 2
-#define num_colours 5
-#define num_cards 5
-#define max_number 5
-#define max_hints 8
+#define NUM_PLAYERS 2
+#define NUM_COLOURS 5
+#define NUM_CARDS 5
+#define MAX_NUMBER 5
+#define MAX_HINTS 8
 
 // TODO: Create enum of colors would make it more readable
-
+//enum colours {green = *, red = +, $, #, @,X};
 int hints = 8;
 int bombs = 4;
-char shapes[num_colours+1] = {'*', '+', '$', '#', '@','X'};
-int cards_available[num_colours][max_number] = {{3,2,2,2,1},{3,2,2,2,1},{3,2,2,2,1},{3,2,2,2,1},{3,2,2,2,1}};
-int player_cards[num_players][num_cards][2] = {0}; // TODO: cards should be represented by a struct with: {int number, Color color}, not a two item array
-int cards_played[num_cards] = {0};
+char shapes[NUM_COLOURS+1] = {'*', '+', '$', '#', '@','X'};
+int cards_available[NUM_COLOURS][MAX_NUMBER] = {{3,2,2,2,1},{3,2,2,2,1},{3,2,2,2,1},{3,2,2,2,1},{3,2,2,2,1}};
+int player_cards[NUM_PLAYERS ][NUM_CARDS][2] = {0}; // TODO: cards should be represented by a struct with: {int number, Color color}, not a two item array
+int cards_played[NUM_CARDS] = {0};
 bool game_over = false;
 int cards_left = 40; // TODO: Cards left should come from cards_available, not a seperate number
 
-// TODO: I am not a fun of this function. Assigning cards should be as simple as:
-//      For each player
-//          for each card
-//              draw_new(player);
-// And thats it. Centralize drawing a card to one place
-//randomly assign player cards
-void assign_cards()
-{
-    for (int i = 0; i<num_players; i++)
-    {
-        for (int j = 0; j<num_cards; j++)
-        {
-            int colour = rand() % num_colours;
-            int number = rand() % max_number;
-            while(cards_available[colour][number] == 0)
-            {
-                colour = rand() % num_colours;
-                number = rand() % max_number;
-            }
-            player_cards[i][j][0] = colour;
-            player_cards[i][j][1] = number;
-            cards_available[colour][number] --;
-        }
-    }
-}
-
-//print current player cards and which cards have been placed down
-void print_cards()
-{
-    for (int i = 0; i < num_players; i++)
-    {
-        printf("Player %d's cards:\n", i + 1);
-        for (int j = 0; j < num_cards; j++)
-        {
-            // TODO: Instead of a bunch of spaces, you should use \t which will indent it.
-            printf("  Card %d: %c%d\n", j + 1, shapes[player_cards[i][j][0]], player_cards[i][j][1] + 1);
-        }
-        printf("\n");
-    }
-    printf("Cards played:\n");
-    for (int i = 0; i < num_colours; i++)
-    {
-        // TODO: Indent the cards here as well
-        printf("%c: %d\n", shapes[i], cards_played[i]);
-    }
-}
-
-// TODO: I can understand a reason to only draw into the 5th position (Kind of more similar to real life where you
-// put your new card on the end of your hand) but this complicates the system. I would prefer if you pass into the function
-// the index to draw a new card into
 
 //draws new card into 5th card position of player
-void draw_new(int player)
+void draw_new(int player, int card)
 {
     // TODO: This works for drawing but this is flawed as a deck because it is not correctly random.
     // You have an equal chance to get any card that is still in the deck BUT it is not weighted by the
@@ -94,37 +44,63 @@ void draw_new(int player)
     // 50% chance to get a 1 and 50% chance to get a 5.
     // TODO: Fix the randomness ( I can give suggestions if you like, but I'll leave it
     // to you to decide if you have a way you want to implement it )
-    int colour = rand() % num_colours;
-    int number = rand() % max_number;
+    int colour = rand() % NUM_COLOURS;
+    int number = rand() % MAX_NUMBER;
     while(cards_available[colour][number] == 0)
     {
-        colour = rand() % num_colours;
-        number = rand() % max_number;
+        colour = rand() % NUM_COLOURS;
+        number = rand() % MAX_NUMBER;
     }
-    player_cards[player][num_cards-1][0] = colour;
-    player_cards[player][num_cards-1][1] = number;
+    player_cards[player][card][0] = colour;
+    player_cards[player][card][1] = number;
     cards_available[colour][number] --;
     cards_left --;
-
 }
 
-// TODO: If you fix the draw_new above to have a parameter of which card to
+//randomly assign player cards
+void assign_cards()
+{
+    for (int i = 0; i<NUM_PLAYERS ; i++)
+    {
+        for (int j = 0; j<NUM_CARDS; j++)
+        {
+            draw_new(i,j);
+        }
+    }
+}
+
+//print current player cards and which cards have been placed down
+void print_cards()
+{
+    for (int i = 0; i < NUM_PLAYERS ; i++)
+    {
+        printf("Player %d's cards:\n", i + 1);
+        for (int j = 0; j < NUM_CARDS; j++)
+        {
+            printf("\tCard %d: %c%d\n", j + 1, shapes[player_cards[i][j][0]], player_cards[i][j][1] + 1);
+        }
+        printf("\n");
+    }
+    printf("Cards played:\n");
+    for (int i = 0; i < NUM_COLOURS; i++)
+    {
+        printf("\t%c: %d\n", shapes[i], cards_played[i]);
+    }
+}
+
+
+
 //discards selected card of selected player
 void discard (int player, int card)
 {
-    for(int i = card; i<num_cards; i++)
-    {
-        player_cards[player][i][0] = player_cards[player][i+1][0];
-        player_cards[player][i][1] = player_cards[player][i+1][1];
-    }
     if(cards_left == 0)
     {
         printf("No more cards left...");
-        player_cards[player][num_cards-1][0] = num_colours;
-        player_cards[player][num_cards-1][1] = -1;
+        player_cards[player][NUM_CARDS-1][0] = NUM_COLOURS;
+        player_cards[player][NUM_CARDS-1][1] = -1;
     }
     else
-        draw_new(player);
+        draw_new(player,card);
     if(!game_over)
         print_cards();
 }
@@ -138,7 +114,7 @@ void play(int player, int card)
         printf("Good play!\n");
         cards_played[player_cards[player][card][0]] ++;
         //did the players win?
-        if(cards_played[0]==max_number && cards_played[1]==max_number && cards_played[2]==max_number && cards_played[3]==max_number && cards_played[4]==max_number)
+        if(cards_played[0]==MAX_NUMBER && cards_played[1]==MAX_NUMBER && cards_played[2]==MAX_NUMBER && cards_played[3]==MAX_NUMBER && cards_played[4]==MAX_NUMBER)
         {
             printf("you win\n");
             game_over = true;
@@ -181,11 +157,11 @@ int main()
             scanf("%d", &card_num);
             play(current_player,card_num-1);
             current_player ++;
-            current_player = current_player%num_players;
+            current_player = current_player%NUM_PLAYERS ;
         }
         else if (selection == 2)
         {
-            if(hints == max_hints)
+            if(hints == MAX_HINTS)
             {
                 printf("You cannot discard, try again\n");
             }
@@ -196,7 +172,7 @@ int main()
                 scanf("%d", &card_num);
                 discard (current_player, card_num-1);
                 current_player ++;
-                current_player = current_player%num_players;
+                current_player = current_player%NUM_PLAYERS ;
                 hints++;
             }
         }
@@ -211,7 +187,7 @@ int main()
                 hints --;
                 printf("Give ur hint bestie!\n");
                 current_player ++;
-                current_player = current_player%num_players;
+                current_player = current_player%NUM_PLAYERS ;
             }
         }
         else
